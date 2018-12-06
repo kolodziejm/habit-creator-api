d280c27b369e90c7383a3da4f3bae617b47c5b29
Marcin Kołodziej <marcinkolodziej15@gmail.com>
2018-12-06 17:51:47 +0100

    Initial server setup

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+
+const db = require('./config/keys').mongoURI;
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(passport.initialize());
+
+mongoose.connect(db)
+  .then(res => {
+    app.listen(port);
+
+  })
+  .catch(err => console.log(err));
