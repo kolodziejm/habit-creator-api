d1474cab1bb14089612a52923ca93b5487c59526
Marcin Kołodziej <marcinkolodziej15@gmail.com>
2018-12-11 17:43:45 +0100

    fixed error response consistency

--- routes/auth.js
+++ routes/auth.js
@@ -30,7 +30,7 @@ router.post('/register', [
   try {
     const user = await User.findOne({ username });
     if (user) {
-      return res.status(400).json({ param: 'username', msg: 'User with that name already exists' })
+      return res.status(400).json({ errObj: { username: 'User with that name already exists' } });
     }
     const hashedPw = await bcrypt.hash(password, 12);
     const newUser = new User({
@@ -52,13 +52,22 @@ router.post('/login', [
   check('username').trim().isAlphanumeric().isLength({ min: 3, max: 30 }).withMessage('Provided username is incorrect'),
   check('password').isLength({ min: 5 }).withMessage('Provided password is incorrect')
 ], async (req, res, next) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    const errArr = errors.array();
+    const errObj = createErrorObj(errArr);
+    console.log(errObj);
+    return res.status(400).json({ errObj });
+  }
+
   const { username, password } = req.body;
   try {
     const user = await User.findOne({ username });
-    if (!user) return res.status(404).json({ msg: 'User doesn\'t exist' });
+    if (!user) return res.status(404).json({ errObj: { username: 'User doesn\'t exist' } });
 
     const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) return res.status(400).json({ msg: 'Wrong password' });
+    if (!isMatch) return res.status(400).json({ errObj: { password: 'Wrong password' } });
 
     const payload = {
       userId: user._id,
