426b288d52d8b785de7e25ba47fb2445e9d05b0e
Marcin Kołodziej <marcinkolodziej15@gmail.com>
2018-12-23 20:18:33 +0100

    achievement router, protected get route

--- routes/achievements.js
+++ routes/achievements.js
@@ -2,9 +2,21 @@ const express = require('express');
 const passport = require('passport');
 
 const User = require('../models/User');
+const Achievement = require('../models/Achievement');
 
 const router = express.Router();
 
-// GET
+// GET /api/achievements
+// Private
+router.get('/', passport.authenticate('jwt', { session: false }), async (req, res, next) => {
+  try {
+    const achievements = await Achievement.find();
+    res.json(achievements);
+  } catch (err) {
+    res.status(404).json({
+      msg: 'Couldn\'t fetch habits'
+    });
+  }
+});
 
 module.exports = router;
