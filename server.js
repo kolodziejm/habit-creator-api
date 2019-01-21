114f1b2314c3f98a93c839c19de2751ddc723c03
Marcin Kołodziej <marcinkolodziej15@gmail.com>
2019-01-21 18:23:06 +0100

    set up cors settings

--- server.js
+++ server.js
@@ -16,7 +16,12 @@ const db = require('./config/keys').mongoURI;
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+const corsOptions = {
+  origin: 'https://kolodziejm.github.io',
+  optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
